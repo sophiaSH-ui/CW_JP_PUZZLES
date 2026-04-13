e2966b3022e7627ca5e027e73b59a73bca93d199
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2026-04-13 21:48:10 +0300

    Create Command directory and add empty file placeholders

---
+++ CW_JP_PUZZLES/CW_JP_PUZZLES/Common/GameResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_JP_PUZZLES.Common
+{
+    internal class GameResult
+    {
+    }
+}
--- CW_JP_PUZZLES/CW_JP_PUZZLES/obj/Debug/net9.0-windows/CW_JP_PUZZLES.AssemblyInfo.cs
+++ CW_JP_PUZZLES/CW_JP_PUZZLES/obj/Debug/net9.0-windows/CW_JP_PUZZLES.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CW_JP_PUZZLES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f15e862927083f3ea882f69eb4e1e8025c170d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f0be40d9c29bce74c3e649855b6ee5b9e10f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("CW_JP_PUZZLES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CW_JP_PUZZLES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
