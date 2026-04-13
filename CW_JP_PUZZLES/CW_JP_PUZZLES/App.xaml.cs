efd7f693e00dbbc1ef38a7a7301511af0fd27db3
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2026-04-13 16:22:35 +0300

    Create a project

---
+++ CW_JP_PUZZLES/CW_JP_PUZZLES/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CW_JP_PUZZLES
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
