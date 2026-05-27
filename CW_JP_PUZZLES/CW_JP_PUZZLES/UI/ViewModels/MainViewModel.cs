62120272c480d61dbe853a4982f37d96de1b0f20
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2026-05-27 23:28:01 +0300

    Fix interface design

--- CW_JP_PUZZLES/CW_JP_PUZZLES/UI/ViewModels/MainViewModel.cs
+++ CW_JP_PUZZLES/CW_JP_PUZZLES/UI/ViewModels/MainViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using CW_JP_PUZZLES.Common;
 using CW_JP_PUZZLES.Data;
 using CW_JP_PUZZLES.Models;
@@ -11,6 +13,8 @@ namespace CW_JP_PUZZLES.UI.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        private readonly Stack<ViewModelBase> _viewHistory = new Stack<ViewModelBase>();
+
         private ViewModelBase _currentViewModel = null!;
         public ViewModelBase CurrentViewModel
         {
@@ -21,8 +25,18 @@ namespace CW_JP_PUZZLES.UI.ViewModels
         private Settings _settings;
         private static readonly string SettingsPath = "Resources/Config/AppSettings.xml";
 
+        public ICommand GoBackCommand { get; }
+        public ICommand ExitCommand { get; }
+        public ICommand NavigateToSettingsCommand { get; }
+        public ICommand NavigateToMenuCommand { get; }
+
         public MainViewModel()
         {
+            GoBackCommand = new RelayCommand(ExecuteGoBack, CanExecuteGoBack);
+            ExitCommand = new RelayCommand(ExecuteExit);
+            NavigateToSettingsCommand = new RelayCommand(_ => NavigateToSettings());
+            NavigateToMenuCommand = new RelayCommand(_ => NavigateToMenu());
+
             _settings = XmlConfigProvider.LoadFromFile<Settings>(SettingsPath);
             SoundService.Instance.ApplySettings(_settings);
 
@@ -32,12 +46,14 @@ namespace CW_JP_PUZZLES.UI.ViewModels
         public void NavigateToMenu()
         {
             SoundService.Instance.PlayMusic(MusicTrack.Menu);
+            _viewHistory.Clear();
             CurrentViewModel = new MainMenuViewModel(this);
         }
 
         public void NavigateToDifficulty(string gameName)
         {
             SoundService.Instance.PlaySfx(SoundEffect.Navigate);
+            if (CurrentViewModel != null) _viewHistory.Push(CurrentViewModel);
             CurrentViewModel = new GameSetupViewModel(this, gameName);
         }
 
@@ -52,18 +68,21 @@ namespace CW_JP_PUZZLES.UI.ViewModels
                 _ => MusicTrack.Menu
             };
             SoundService.Instance.PlayMusic(track);
+            if (CurrentViewModel != null) _viewHistory.Push(CurrentViewModel);
             CurrentViewModel = new GameViewModel(this, config);
         }
 
         public void NavigateToResult(GameResult result, GameConfig config)
         {
             SoundService.Instance.PlaySfx(SoundEffect.Victory);
+            if (CurrentViewModel != null) _viewHistory.Push(CurrentViewModel);
             CurrentViewModel = new ResultViewModel(this, result, config);
         }
 
         public void NavigateToSettings()
         {
             SoundService.Instance.PlaySfx(SoundEffect.Navigate);
+            if (CurrentViewModel != null) _viewHistory.Push(CurrentViewModel);
             CurrentViewModel = new SettingsViewModel(this, _settings, SaveSettings);
         }
 
@@ -72,6 +91,32 @@ namespace CW_JP_PUZZLES.UI.ViewModels
             _settings = updated;
             XmlConfigProvider.SaveToFile(_settings, SettingsPath);
             SoundService.Instance.ApplySettings(_settings);
+        }
+
+        private void ExecuteGoBack(object obj)
+        {
+            if (_viewHistory.Count > 0)
+            {
+                SoundService.Instance.PlaySfx(SoundEffect.Navigate);
+                var previousViewModel = _viewHistory.Pop();
+
+                if (previousViewModel is MainMenuViewModel)
+                {
+                    SoundService.Instance.PlayMusic(MusicTrack.Menu);
+                }
+
+                CurrentViewModel = previousViewModel;
+            }
+        }
+
+        private bool CanExecuteGoBack(object obj)
+        {
+            return _viewHistory.Count > 0;
+        }
+
+        private void ExecuteExit(object obj)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
